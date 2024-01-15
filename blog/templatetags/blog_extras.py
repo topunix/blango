8b4f14583d455dc26dab29950dc3c7d208922ed2
Michael Accardo <friendly.nickel@gmail.com>
2024-01-15 20:49:29 +0000

    Moved templatetags directory

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,32 @@
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
+
+
+
+from django import template
+register = template.Library()
+
+from django.contrib.auth import get_user_model
+user_model = get_user_model()
+
+@register.filter
+def author_details(author):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    if author.first_name and author.last_name:
+        name = escape(f"{author.first_name} {author.last_name}")
+    else:
+        name = escape(f"{author.username}")
+
+    if author.email:
+        email = escape(author.email)
+        prefix = f'<a href="mailto:{email}">'
+        suffix = "</a>"
+    else:
+        prefix = ""
+        suffix = ""
+
+    return mark_safe(f"{prefix}{name}{suffix}")
