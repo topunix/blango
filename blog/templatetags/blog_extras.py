3d070bac35551c73de8e74dfcf357a4de8cff614
Michael Accardo <friendly.nickel@gmail.com>
2024-01-15 22:43:33 +0000

    Finish custom filters

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,7 +1,6 @@
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
-
-
+from django.utils.html import format_html
 
 
 from django import template
@@ -11,22 +10,24 @@ from django.contrib.auth import get_user_model
 user_model = get_user_model()
 
 @register.filter
-def author_details(author):
+def author_details(author, current_user=None):
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
 
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
     if author.first_name and author.last_name:
-        name = escape(f"{author.first_name} {author.last_name}")
+        name = f"{author.first_name} {author.last_name}"
     else:
-        name = escape(f"{author.username}")
+        name = f"{author.username}"
 
     if author.email:
-        email = escape(author.email)
-        prefix = f'<a href="mailto:{email}">'
-        suffix = "</a>"
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
     else:
         prefix = ""
         suffix = ""
 
-    return mark_safe(f"{prefix}{name}{suffix}")
+    return format_html('{}{}{}', prefix, name, suffix)
